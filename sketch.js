472eb689a72e6f30e4129625cc8486596de0748d
Adrian Bakke <adrian.bakke@yahoo.no>
2020-02-29 17:28:31 +0100

    add ground

--- sketch.js
+++ sketch.js
@@ -5,12 +5,18 @@ function setup() {
   let img4 = loadImage('images/Robot_4.png'); // Load the image
   let robotImgs = [img1, img3, img2, img3, img4]
   createCanvas(innerWidth, innerHeight);
-  robot = new Robot(50, 400, robotImgs);
+  robot = new Robot(0, 400+135, robotImgs);
+  ground = new Ground();
 }
 
 function draw() {
   frameRate(30);
   background(255,255,255);
+  ground.display();
+  robot.groundy = ground.Y(robot.x);
+  if ((((PI/2)-0.1)<ground.angle(robot.x)<((PI/2)+0.1)) && !robot.jump) {
+    robot.y = robot.groundy;
+  }
   robot.display();
 }
 
@@ -18,7 +24,7 @@ class Robot {
   constructor(x, y, imgs) {
     this.x = x;
     this.y = y;
-    this.levely = y;
+    this.groundy = y;
     this.imgs = imgs;
     this.imgNum = 0;
     this.velocity = -40;
@@ -36,9 +42,9 @@ class Robot {
   moveY() {
     this.velocity += this.gravity; 
     this.y += this.velocity;
-    if (this.y > this.levely) {
+    if (this.y > this.groundy) {
       this.jump = false;
-      this.y = this.levely;
+      this.y = this.groundy;
       this.velocity = -40;
       this.imgNum = 1;
     }
@@ -48,7 +54,7 @@ class Robot {
     if (this.jump) {
       this.imgNum = 4;
     }
-    image(this.imgs[this.imgNum], this.x, this.y);
+    image(this.imgs[this.imgNum], this.x-50, this.y-135);
     moveRobot();
   }
 }
@@ -65,5 +71,27 @@ function moveRobot() {
   if (keyIsDown(UP_ARROW) || robot.jump) {
     robot.jump = true;
     robot.moveY();
+  }
+}
+
+class Ground {
+  Y(x, sign=1) {
+    return (500 + 40 * sin(x/100));
+  }
+
+  slope(x) {
+    return ((40/100) * cos(x/100));
+  }
+
+  angle(x) {
+    return acos(0.1/(this.slope(x)+this.slope(x+0.1)));
+  }
+
+  display() {
+    beginShape();
+    for (let i = 0; i < 1000; i++) {
+      curveVertex(i, this.Y(i));
+    }
+    endShape();
   }
 }
